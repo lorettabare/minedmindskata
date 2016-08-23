fa0599fd08c9b5472e3553b00ac5a4193a40bcc8
lorettabare <barh0514@gmail.com>
2016-08-23 12:02:52 -0700

    changed the 1 to 2

--- testminedmindsfunction.rb
+++ testminedmindsfunction.rb
@@ -6,9 +6,11 @@ class TestMinedMindsFunction < Minitest::Test
 		assert_equal(1, 1)
 	end
 	def test_1_returns_1
-	assert_equal(1, mined_minds(1))
+	  assert_equal(1, mined_minds(1))
 	end
-
+    def test_2_returns_2
+	  assert_equal(2, mined_minds(2))
+    end
 end
 
 	
