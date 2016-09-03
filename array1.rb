e02eef585387067fdfe3df323c1a0c52502a8c90
lorettabare <barh0514@gmail.com>
2016-09-03 18:38:55 -0400

    commit of exercise 5

--- array1.rb
+++ array1.rb
@@ -12,4 +12,31 @@ my_array
 end				
 
 
+def array_mined_minds
+	my_array = []
+	(1..100).each do |number|
+	if number == 5
+		number = "minds"
+	else
+	number = number
+	end
+	my_array << number
+	end
+my_array
+end
+
+def array_mined_minds
+	my_array = []
+	(1..100).each do |number|
+	if number == % (3,5) 
+		number = "minedminds"
+	else
+	number = number
+	end
+	my_array << number
+	end
+my_array
+end
+
+
 
--- array1test.rb
+++ array1test.rb
@@ -11,5 +11,32 @@ def test_3_returns_mined
 	results = array_mined_minds
 	assert_equal("mined" , results[2])
 	end
+end
 
+require "minitest/autorun"
+require_relative "array1.rb"
+class TestArrayFunction <Minitest::Test
+	def test_array_with_100_elements
+		results = array_mined_minds
+		assert_equal(100, results.length)
+	end
+
+	def test_5_returns_minds
+		results = array_mined_minds
+		assert_equal("minds" , results[2])
+		end
+	end
+
+require "minitest/autorun"
+require_relative "array1.rb"
+class TestArrayFunction <Minitest::Test
+	def test_arry_with_100_elements
+		results = array_mined_minds
+		assert_equal(100, results.length)
+	end
+
+def test_3_5_returns_minedminds
+	results = array_mined_minds
+	assert_equal("minedminds" , results[2])
+	end
 end
