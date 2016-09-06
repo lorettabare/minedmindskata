a7ebb8074a5d9d5ed4407aa878d1805df9709b63
lorettabare <barh0514@gmail.com>
2016-09-06 11:40:54 -0400

    first test pass in array 1

--- array1.rb
+++ array1.rb
@@ -1,42 +1,24 @@
 def array_mined_minds
 	my_array = []
 	(1..100).each do |number|
-	if number == 3
-		number = "mined"
-	else
-	number = number
+		if number % 15 == 0
+			number = "minedminds"	
+
+		elsif 
+			number % 3 == 0
+			number = "mined"
+		elsif 
+			number % 5 == 0
+			number = "minds"
+		else
+			number = number
+		end
+	my_array.push(number)
 	end
-	my_array << number
-	end
-my_array
+	my_array
 end				
+	
 
-
-def array_mined_minds
-	my_array = []
-	(1..100).each do |number|
-	if number == 5
-		number = "minds"
-	else
-	number = number
-	end
-	my_array << number
-	end
-my_array
-end
-
-def array_mined_minds
-	my_array = []
-	(1..100).each do |number|
-	if number == % (3,5) 
-		number = "minedminds"
-	else
-	number = number
-	end
-	my_array << number
-	end
-my_array
-end
 
 
 
--- array1test.rb
+++ array1test.rb
@@ -3,40 +3,31 @@ require_relative "array1.rb"
 class TestArrayFunction <Minitest::Test
 	def test_array_with_100_elements
 			results = array_mined_minds
-			assert_equal(100, results.length)
+			assert_equal(100, results.count)
+	end
+
+	def test_array_3_element 
+		results = array_mined_minds
+		assert_equal("mined", results[2])
+	end
+	def test_array_5_element 
+		results = array_mined_minds
+		assert_equal("minds", results[4])
+	end
+	def test_array_15_element
+		results = array_mined_minds
+		assert_equal("minedminds", results[14])
+	end
+
+	def test_array_33_element
+		results = array_mined_minds
+		assert_equal("mined", results[32])
 	end
 
 
-def test_3_returns_mined
-	results = array_mined_minds
-	assert_equal("mined" , results[2])
-	end
+
+
 end
+	
 
-require "minitest/autorun"
-require_relative "array1.rb"
-class TestArrayFunction <Minitest::Test
-	def test_array_with_100_elements
-		results = array_mined_minds
-		assert_equal(100, results.length)
-	end
 
-	def test_5_returns_minds
-		results = array_mined_minds
-		assert_equal("minds" , results[2])
-		end
-	end
-
-require "minitest/autorun"
-require_relative "array1.rb"
-class TestArrayFunction <Minitest::Test
-	def test_arry_with_100_elements
-		results = array_mined_minds
-		assert_equal(100, results.length)
-	end
-
-def test_3_5_returns_minedminds
-	results = array_mined_minds
-	assert_equal("minedminds" , results[2])
-	end
-end
